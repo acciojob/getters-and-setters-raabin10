1538f142191a26d8f22c3d15d7999f80819f62ae
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2024-06-08 10:53:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 class Person {
-  constructor(name, age) {
-    this._name = name;
-    this._age = age;
-  }
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
 
-  get name() {
-    return this._name;
-  }
+    get name() {
+        return this._name;
+    }
 
-  get age() {
-    return this._age;
-  }
-
-  introduce() {
-    console.log(`Hello, my name is ${this._name} and I am ${this._age} years old.`);
-  }
+    set age(age) {
+        if (typeof age === 'number' && age > 0) {
+            this._age = age;
+        } else {
+            throw new Error("Age must be a positive number");
+        }
+    }
 }
 
 class Student extends Person {
-  study() {
-    console.log(`${this._name} is studying.`);
-  }
+    study() {
+        console.log(`${this.name} is studying`);
+    }
 }
 
 class Teacher extends Person {
-  teach() {
-    console.log(`${this._name} is teaching.`);
-  }
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
 }
 
 // Do not change the code below this line
